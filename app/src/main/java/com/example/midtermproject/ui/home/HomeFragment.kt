1786ca330b84e8df15610258b39bc86afbb65320
Sergio <smontufar250@gmail.com>
2024-03-20 22:33:20 -0400

    committing project to github

---
+++ app/src/main/java/com/example/midtermproject/ui/home/HomeFragment.kt
@@ -0,0 +1,49 @@
+package com.example.midtermproject.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+//import android.widget.Button
+//import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.midtermproject.databinding.FragmentDashboardBinding
+import com.example.midtermproject.ui.home.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    private lateinit var timerViewModel: HomeViewModel
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Instantiate the ViewModel scoped to the Fragment
+        timerViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        timerViewModel.time.observe(viewLifecycleOwner) {
+            binding.textDashboard.text = formatTime(it)
+        }
+
+        binding.startButton.setOnClickListener { timerViewModel.startTimer() }
+        binding.pauseButton.setOnClickListener { timerViewModel.stopPauseTimer() }
+        binding.resetButton.setOnClickListener { timerViewModel.resetTimer() }
+    }
+
+    private fun formatTime(seconds: Long): String {
+        val hours = seconds / 3600
+        val minutes = (seconds % 3600) / 60
+        val secs = seconds % 60
+        return String.format("%02d:%02d:%02d", hours, minutes, secs)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/midtermproject/ui/home/HomeViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.midtermproject.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.Timer
+import kotlin.concurrent.scheduleAtFixedRate
+
+class HomeViewModel : ViewModel() {
+    private var timer: Timer? = null
+    private val _time = MutableLiveData<Long>() // might change to long
+
+    val time: LiveData<Long>
+        get() = _time
+
+    init {
+        _time.value = 0
+    }
+
+    fun startTimer() {
+        timer = Timer().apply {
+            scheduleAtFixedRate(0, 1000) {
+                _time.postValue(_time.value!! + 1)
+            }
+        }
+    }
+
+    fun stopPauseTimer() {
+        timer?.cancel()
+    }
+
+    fun resetTimer() {
+        stopPauseTimer()
+        _time.postValue(0)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+}
