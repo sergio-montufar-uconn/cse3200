35d76e237e9395d1ea4c5bc24e9117872cdf9812
Sergio <smontufar250@gmail.com>
2024-05-01 03:52:49 -0400

    added video to github:

--- FinalProject/app/src/main/java/com/example/k2024_04_21_livedata_volley/MainActivity.kt
+++ FinalProject/app/src/main/java/com/example/k2024_04_21_livedata_volley/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.ImageRequest
@@ -17,53 +18,73 @@ import com.example.k2024_04_21_livedata_volley.view_models.UrlViewModel
 import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var container: ConstraintLayout
     private val gson = Gson()
     private val metPublicDomainUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
     private var imageData : JSON_MetMuseum? = null
     private lateinit var volleyQueue: RequestQueue
-
-
+    private var currentImageIndex = 0
+    private val backgroundImages = intArrayOf(R.drawable.museum1,
+                                              R.drawable.museum2,
+                                              R.drawable.museum3,
+                                              R.drawable.mueseum4,
+                                              R.drawable.museum5)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         volleyQueue = Volley.newRequestQueue(this)
 
-        val uriViewModel: UrlViewModel by viewModels<UrlViewModel>()
+        val uriViewModel: UrlViewModel by viewModels()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loadImageMetaDataButton.setOnClickListener{
+        container = findViewById(R.id.container)
+        binding.changeBackground.setOnClickListener {
+            currentImageIndex = (currentImageIndex + 1) % backgroundImages.size
+            container.setBackgroundResource((backgroundImages[currentImageIndex]))
+        }
+
+
+        binding.nextImageButton.setOnClickListener {
             val nextIndex = uriViewModel.nextImageNumber()
             val metUrl = metPublicDomainUrl + nextIndex.toString()
             uriViewModel.setMetaDataUrl(metUrl)
+
             val jsonObjectRequest = JsonObjectRequest(
                 Request.Method.GET,
                 uriViewModel.getMetaDataUrl(),
                 null,
                 { response ->
                     imageData = gson.fromJson(response.toString(), JSON_MetMuseum::class.java )
-                    uriViewModel.setImageUrl(imageData?.primaryImage.toString() ?: "Foobar")
+                    val title = imageData?.title
+                    val artist = imageData?.artistDisplayName
+                    val region = imageData?.region
+                    val culture = imageData?.culture
+
+                    binding.title.text = if (title != "") "$title" else "N/A"
+                    binding.artist.text = if (artist != "") "Artist - $artist" else "N/A"
+                    binding.region.text = if (region != "") "Region - $region" else "N/A"
+                    binding.culture.text = if (culture != "") "Culture - $culture" else "N/A"
+
+
+                    val image = imageData?.primaryImage.toString()
+                    if (image.isNotEmpty()) {
+                        uriViewModel.setImageUrl(image)
+                        val imageRequest = ImageRequest(
+                            image,
+                            { response: Bitmap ->
+                                binding.imageView.setImageBitmap(response)
+                            },
+                            0,0,
+                            ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
+                            { error ->  Log.i("PGB", "Error: ${error}" )})
+                        volleyQueue.add(imageRequest)
+                    }
                 },
                 { error ->  Log.i("PGB" ,"Error: ${error}") })
             volleyQueue.add(jsonObjectRequest)
-        }
-
-        binding.nextImageButton.setOnClickListener {
-            val urlIMAGE = uriViewModel.getImageUrl().toString()
-            val imageRequest = ImageRequest(
-                urlIMAGE,
-                { response: Bitmap ->
-                    binding.imageView.setImageBitmap(response)
-                },
-                0,0,
-                ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
-                { error ->  Log.i("PGB", "Error: ${error}" )})
-
-            volleyQueue.add(imageRequest)
         }
     }
 }
